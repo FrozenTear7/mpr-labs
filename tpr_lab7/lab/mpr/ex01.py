395a6174fb9d5efc0347fde4fb5af074bbb24837
Paweł Mendroch <pawelmendroch7@gmail.com>
2020-04-20 10:38:53 +0200

    Add lab7&8

---
+++ tpr_lab7/lab/mpr/ex01.py
@@ -0,0 +1,26 @@
+import os
+import time
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    if os.name == 'nt':
+        print('detected windows, setting HADOOP_HOME')
+        os.environ['HADOOP_HOME'] = 'C:/hadoop/hadoop-2.7.1'
+
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("Word Count") \
+        .getOrCreate()
+    sc = spark.sparkContext
+
+    text_file = sc.textFile('text')
+    counts = text_file.flatMap(lambda line: line.split(' ')) \
+        .map(lambda word: (word, 1)) \
+        .reduceByKey(lambda a, b: a + b) \
+        .sortBy(lambda x: x[1], False)
+
+    counts.saveAsTextFile('output01')
+
+    time.sleep(300)
+
+    spark.stop()
