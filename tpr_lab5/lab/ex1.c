b32a613c3532a57b99e80b2b1ccd14145a18e5a5
pmendroc <pmendroc@lab-shell.ipa.ki.agh.edu.pl>
2020-04-22 15:50:28 +0200

    Clean lab5

--- tpr_lab5/lab/ex1.c
+++ tpr_lab5/lab/ex1.c
@@ -8,56 +8,23 @@ int main(int argc, char **argv)
     time_t t;
     srand((unsigned)time(&t));
 
-    int dim = atoi(argv[1]);
+    int vec_size = atoi(argv[1]);
+    int max_rand = atoi(argv[2]);
 
-    int *matrix = (int *)malloc(dim * dim * sizeof(int));
-    int *vector = (int *)malloc(dim * sizeof(int));
-    int *result = (int *)malloc(dim * sizeof(int));
-
-    // Wypelniam macierz losowymi intami
-    int i, j;
-    for (i = 0; i < dim; i++)
-        for (j = 0; j < dim; j++)
-            *(matrix + i * dim + j) = rand() % 50;
-
-    // A nastepnie wektor oraz wypelnienie wynikowego wektora zerami
-    for (i = 0; i < dim; i++)
-    {
-        *(vector + i) = rand() % 50;
-        *(result + i) = 0;
-    }
-
-    // printf("Matrix\n");
-    // for (i = 0; i < dim; i++)
-    // {
-    //     for (j = 0; j < dim; j++)
-    //         printf("%d ", *(matrix + i * dim + j));
-
-    //     printf("\n");
-    // }
-
-    // printf("\nVector\n");
-    // for (i = 0; i < dim; i++)
-    //     printf("%d ", *(vector + i));
-
-    // printf("\n\n");
+    double *vector = (double *)malloc(vec_size * sizeof(double));
 
     double start_timer = omp_get_wtime();
 
+    // Losuję wartości w odpowiednim zakresie do wektora
+    int i;
 #pragma omp parallel for
-    for (i = 0; i < dim; i++)
-#pragma omp parallel for
-        for (j = 0; j < dim; j++)
-            *(result + i) += *(matrix + i * dim + j) * *(vector + j);
+    for (i = 0; i < vec_size; i++)
+    {
+        *(vector + i) = rand() * max_rand;
+    }
 
     double end_timer = omp_get_wtime();
-    printf("Calculations took: %lfs, for %d dim and %d threads\n", end_timer - start_timer, dim, omp_get_max_threads());
-
-    // printf("Result\n");
-    // for (i = 0; i < dim; i++)
-    //     printf("%d ", *(result + i));
-
-    // printf("\n\n");
+    printf("Calculations took: %lfs, for %d size and %d threads\n", end_timer - start_timer, vec_size, omp_get_max_threads());
 
     return 0;
 }
